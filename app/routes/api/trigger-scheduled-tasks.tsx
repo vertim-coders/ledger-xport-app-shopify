f4bc0fb7137da3f6448c5bdfe70090ccb0e69606
Bernadoadk <bernado.adikpeto@epitech.eu>
2025-07-15 22:33:55 +0100

    UI update on responsive dashboard

--- app/routes/api/trigger-scheduled-tasks.tsx
+++
@@ -1,7 +0,0 @@
-import { json } from '@remix-run/node';
-import { processScheduledTasks } from '../../workers/scheduledReports';
-
-export const loader = async () => {
-  await processScheduledTasks();
-  return json({ status: 'ok' });
-}; 
